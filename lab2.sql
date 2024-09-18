d4922a68edefd78a6894f1eb104459845e40ddbf
dul-cet <a_moldir@kbtu.kz>
2024-09-18 16:48:47 +0500

    7

--- lab2.sql
+++ lab2.sql
@@ -1,5 +1,2 @@
-INSERT INTO countries (country_name, region_id, population)
-VALUES
-    ('France', 3, 67000000),
-    ('Germany', 3, 83000000),
-    ('Italy', 3, 60000000);
+ALTER TABLE countries
+ALTER COLUMN country_name SET DEFAULT 'Kazakhstan';
