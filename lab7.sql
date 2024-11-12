8f04ec12d08ac99ce916a6ab67dd1bf6113de358
dul-cet <a_moldir@kbtu.kz>
2024-11-12 22:52:10 +0500

    lab7

--- lab7.sql
+++ lab7.sql
@@ -8,7 +8,7 @@ CREATE INDEX idx_employees_name_surname ON employees (name, surname);
 CREATE UNIQUE INDEX idx_employees_salary_range ON employees (salary);
 
 -- 4. Create an index on the first 4 characters of employees' name
-CREATE INDEX idx_employees_name_subst ON employees (substring(name from 1 for 4));
+CREATE INDEX idx_employees_name_substring ON employees (substring(name from 1 for 4));
 
 -- 5. Create indexes for joining employees and departments on department_id, with filtering on budget and salary
 CREATE INDEX idx_emp_dept_budget_salary ON employees (department_id, salary);
